172c310e42a9fc3c1f743477681e80c8f4dc2719
“Kumaratul” <getintouch.atul@gmail.com>
2026-07-30 23:14:42 +0530

    Re-enable subdirectory base and routing paths for /elvohr/

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/elvohr">
       <ScrollToTop />
       <div className="app-container">
         <Navbar />
